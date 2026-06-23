222f1674579ec26ab24ecc26861a7df16b4a1afe
ingsfabrizi-code <ing.s.fabrizi@gmail.com>
2026-06-23 11:39:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # ⚠️ EMAIL RIPULITA E CORRETTA
-IL_TUO_INDIRIZZO_EMAIL = "sicurezza@sigismondicostruzioni.com"
+IL_TUO_INDIRIZZO_EMAIL = "ing.s.fabrizi@gmail.com"
 
 # Configurazione della pagina
 st.set_page_config(page_title="Consulenza Edile Smart", page_icon="🏗️")
